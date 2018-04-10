a5ce9579d850f03f93b4f86f8c43fa9155653552
Max lab pc <tmaxletitgo@gmail.com>
2018-04-10 15:16:15 -0500

    finished

--- app/src/main/java/com/a986415/lab7/MainActivity.java
+++ app/src/main/java/com/a986415/lab7/MainActivity.java
@@ -1,5 +1,6 @@
 package com.a986415.lab7;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -10,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
         WebView mWebView =  findViewById(R.id.webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("file:///assets/index.html");
+        mWebView.loadUrl("file:///android_asset/index.html");
     }
 }
